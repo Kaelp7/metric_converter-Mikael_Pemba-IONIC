4342b6157ea8f5a693cd99e15df324f3fa13696b
MikaelPemba7 <109562254+Kaelp7@users.noreply.github.com>
2023-09-16 16:34:09 +0800

    Update folder.page.ts

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -9,12 +9,57 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class FolderPage implements OnInit {
-  public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
+
+export class FolderPage {
+  input: any;
+  result: any;
+  inputType: any;
+  resultType: any;
+  inputTypeValue: any;
+  resultTypeValue: any;
+
   constructor() {}
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  ionViewDidEnter() {
+    this.input = document.getElementById('input');
+    this.result = document.getElementById('result');
+    this.inputType = document.getElementById('inputType');
+    this.resultType = document.getElementById('resultType');
+
+    this.input.addEventListener("keyup", this.myResult.bind(this));
+    this.inputType.addEventListener("change", this.myResult.bind(this));
+    this.resultType.addEventListener("change", this.myResult.bind(this));
+
+    this.inputTypeValue = this.inputType.value;
+    this.resultTypeValue = this.resultType.value;
+  }
+
+  myResult() {
+    this.inputTypeValue = this.inputType.value;
+    this.resultTypeValue = this.resultType.value;
+
+    if (this.inputTypeValue === "meter" && this.resultTypeValue === "kilometer") {
+      this.result.value = Number(this.input.value) * 0.001;
+    } else if (this.inputTypeValue === "meter" && this.resultTypeValue === "centimeter") {
+      this.result.value = Number(this.input.value) * 100;
+    } else if (this.inputTypeValue === "meter" && this.resultTypeValue === "meter") {
+      this.result.value = this.input.value;
+    }
+
+    if (this.inputTypeValue === "kilometer" && this.resultTypeValue === "meter") {
+      this.result.value = Number(this.input.value) * 1000;
+    } else if (this.inputTypeValue === "kilometer" && this.resultTypeValue === "centimeter") {
+      this.result.value = Number(this.input.value) * 100000;
+    } else if (this.inputTypeValue === "kilometer" && this.resultTypeValue === "kilometer") {
+      this.result.value = this.input.value;
+    }
+
+    if (this.inputTypeValue === "centimeter" && this.resultTypeValue === "kilometer") {
+      this.result.value = Number(this.input.value) * 0.00001;
+    } else if (this.inputTypeValue === "centimeter" && this.resultTypeValue === "meter") {
+      this.result.value = Number(this.input.value) * 0.01;
+    } else if (this.inputTypeValue === "centimeter" && this.resultTypeValue === "centimeter") {
+      this.result.value = this.input.value;
+    }
   }
 }
